0dc3a8c1d6b57a17f42a148f901c0856edc9682e
Ricardo Krieg <ricardo.krieg@gmail.com>
2013-02-26 17:16:24 -0300

    up

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,10 @@
 class Post < ActiveRecord::Base
+    attr_accessible :body, :excerpt, :title, :author_id, :category_ids
 
-  attr_accessible :body, :excerpt, :title, :author_id, :category
-  validates_presence_of :title, :excerpt, :body, :author
-  validates_associated :author
-  has_many :comments
-  has_and_belongs_to_many :categories
+    belongs_to :author, class_name: "User", :foreign_key => "author_id"
+    has_many :comments
+    has_and_belongs_to_many :categories
 
-  belongs_to :author, class_name: "User", :foreign_key => "author_id"
-
-
+    validates_presence_of :title, :excerpt, :body, :author_id
+    validates_associated :author
 end
